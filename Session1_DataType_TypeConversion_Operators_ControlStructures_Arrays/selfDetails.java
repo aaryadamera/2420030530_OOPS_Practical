32d9ee473b02d5807e457419c9a7b27217fb64ba
aaryadamera <damera.aarya@gmail.com>
2025-08-02 09:37:15 +0530

    Add files via upload

---
+++ Session1_DataType_TypeConversion_Operators_ControlStructures_Arrays/selfDetails.java
@@ -0,0 +1,13 @@
+package labDay1_myproject;
+
+public class selfDetails {              
+    public static void main(String[] args) {
+        String n = "Aarya";
+        String d = "CSE";
+        long r = 2420030530L;            
+        long p = 9392376767L;           
+        String a = "Miyapur";
+
+        System.out.printf("Name: %s%nDepartment: %s%nRoll.no: %d%nPhone.no: %d%nAddress: %s%n",n, d, r, p, a);
+    }
+}
